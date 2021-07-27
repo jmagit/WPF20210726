fff579d6e28813a90ff80ead6e7393646de6746d
Javier <jmartin@grupoloyal.com>
2021-07-27 18:04:13 +0200

    Core

---
+++ Demos.Domains.Core/ObservableBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.Domains.Core
+{
+    public class ObservableBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs ev) {
+            if (ev != null && PropertyChanged != null) {
+                PropertyChanged(this, ev);
+            }
+        }
+
+        public void RaisePropertyChanged(string PropertyName) {
+            OnPropertyChanged(new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+    }
+}
