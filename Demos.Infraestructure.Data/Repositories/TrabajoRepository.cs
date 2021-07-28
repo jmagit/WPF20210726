5038920380a51307eb3414b8642c19126f5f0dd8
Javier <jmartin@grupoloyal.com>
2021-07-28 17:04:59 +0200

    DataSet

--- Demos.Infraestructure.Data/Repositories/TrabajoRepository.cs
+++ Demos.Infraestructure.Data/Repositories/TrabajoRepository.cs
@@ -5,12 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Demos.Infraestructure.Data.Repositories {
     public class TrabajoRepository : ITrabajoRepository {
-        public List<Trabajo> Cargar() {
+        public List<Trabajo> CargarDS() {
             var filePath = @"D:\Cursos\dotnet\WPF20210726\Curso.xlsx";
             var lstTrabajos = new List<Trabajo>();
             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)) {
@@ -38,6 +39,24 @@ namespace Demos.Infraestructure.Data.Repositories {
             }
             return lstTrabajos;
         }
+
+        public List<Trabajo> Cargar() {
+            var filePath = @"D:\Cursos\dotnet\WPF20210726\Curso.xlsx";
+            var lstTrabajos = new List<Trabajo>();
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)) {
+                using (var reader = ExcelReaderFactory.CreateReader(stream)) {
+                    var result = reader.AsDataSet(new ExcelDataSetConfiguration() {
+                        UseColumnDataType = true,
+                        ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration() {
+                            UseHeaderRow = true,
+                        }
+                    });
+                    lstTrabajos = result.Tables[0].AsEnumerable().Select(item => new Trabajo(item.ItemArray[0] as string, item.ItemArray[1] as string,
+                            (double)item.ItemArray[2], (DateTime)item.ItemArray[3])).ToList();
+                }
+            }
+            return lstTrabajos;
+          }
 
     }
 }
